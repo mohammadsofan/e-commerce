d1e913ff0f85fac10308ac8280b0dcb1f776b8d0
mohammadsofan <mohammadsof72@gmail.com>
2024-03-31 18:48:14 +0200

    version 1

--- src/pages/products/components/Pagination/Pagination.jsx
+++ src/pages/products/components/Pagination/Pagination.jsx
@@ -13,7 +13,14 @@ export default function Pagination({ num, getProducts, setCurPage, curPage }) {
         className={`page-link ${curPage == 1 ? "disabled" : ""}`}
         onClick={() => handleOnCLick(curPage - 1)}
       >
-        Previous
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14px"
+          height="14px"
+          viewBox="0 0 20 20"
+        >
+          <path fill="green" d="m4 10l9 9l1.4-1.5L7 10l7.4-7.5L13 1z" />
+        </svg>
       </button>
     </li>
   );
@@ -35,7 +42,14 @@ export default function Pagination({ num, getProducts, setCurPage, curPage }) {
         className={`page-link ${curPage == num ? "disabled" : ""} `}
         onClick={() => handleOnCLick(curPage + 1)}
       >
-        Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="14px"
+          height="14px"
+          viewBox="0 0 20 20"
+        >
+          <path fill="green" d="M7 1L5.6 2.5L13 10l-7.4 7.5L7 19l9-9z" />
+        </svg>
       </button>
     </li>
   );
